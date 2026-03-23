e89a8fff39da49ed1322d5150c9a41cf7f71d2d3
matfeinikolaev <matfei.n@balcony.io>
2026-03-23 13:48:52 -0300

    updated api connection

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.MARKETSTACK_API_KEY
 
-export const API_BASE_URL = '/api'
+export const API_BASE_URL = 'https://corsproxy.io/?http://api.marketstack.com/v1'
 export const API_HEADERS = {
   'X-API-KEY': API_KEY,
   'Content-Type': 'application/json',
--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/final-project/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://api.marketstack.com/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
